3f506caeabf2e2544a338682a747a266aee05627
Ed Zynda <ezynda3@gmail.com>
2022-03-18 20:36:44 +0300

    🔧 clean up tests

--- src/test/MagicStakePool.t.sol
+++ src/test/MagicStakePool.t.sol
@@ -8,25 +8,24 @@ import "../MagicStakePool.sol";
 contract MagicStakePoolTest is BaseTest {
     address private MAGIC_TOKEN = 0x539bdE0d7Dbd336b79148AA742883198BBF60342;
     address private ATLAS_MINE = 0xA0A89db1C899c49F98E6326b764BAFcf167fC2CE;
+    address private MAGIC_WHALE = 0x7B660FBafdADF78aB7bfe83475841712ae774066;
 
     MagicStakePool private pool;
 
     function setUp() public {
         pool = new MagicStakePool(ERC20(MAGIC_TOKEN), AtlasMine(ATLAS_MINE));
+        vm.startPrank(MAGIC_WHALE);
     }
 
     function testUserCanDeposit() public {
-        vm.startPrank(0x7B660FBafdADF78aB7bfe83475841712ae774066); 
         ERC20 magic = ERC20(MAGIC_TOKEN);
         uint256 atlasStartingBalance = magic.balanceOf(ATLAS_MINE);
         magic.approve(address(pool), 100 ether);
         pool.deposit(100 ether);
         assertGt(magic.balanceOf(ATLAS_MINE), atlasStartingBalance);
-        vm.stopPrank();
     }
 
     function testFailWithdrawBeforeMaturity() public {
-        vm.startPrank(0x7B660FBafdADF78aB7bfe83475841712ae774066); 
         ERC20 magic = ERC20(MAGIC_TOKEN);
         uint256 atlasStartingBalance = magic.balanceOf(ATLAS_MINE);
         magic.approve(address(pool), 100 ether);
@@ -34,11 +33,9 @@ contract MagicStakePoolTest is BaseTest {
         
         uint256 depositId = pool.currentDepositId();
         pool.withdraw(depositId);
-        vm.stopPrank();
     }
 
     function testUserCanWithdrawAfterTwoWeeks() public {
-        vm.startPrank(0x7B660FBafdADF78aB7bfe83475841712ae774066); 
         ERC20 magic = ERC20(MAGIC_TOKEN);
         uint256 atlasStartingBalance = magic.balanceOf(ATLAS_MINE);
         magic.approve(address(pool), 100 ether);
@@ -48,7 +45,5 @@ contract MagicStakePoolTest is BaseTest {
 
         uint256 depositId = pool.currentDepositId();
         pool.withdraw(depositId);
-
-        vm.stopPrank();
     }
 }
